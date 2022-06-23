526e761ef19345a7862692bc46c89c11af3cab85
Gabriela Caceres <gagabrielacaceres@gmail.com>
2022-06-23 13:06:38 -0300

    estoy  guardando todo y haciendo un commit

---
+++ index.py
@@ -0,0 +1,12 @@
+''' 
+hice un  git clone de un repositorio que tenia el  git, 
+ahora voy a intentar subir un archivo al repositorio externo (nube)
+luego de  guardar el archivo creado en visual , aprecio la U en verde, y al posicionarme
+sale,  UNTRACKED
+
+VOY A  SOURCE CONTROL y aprete el mas o  no se que y se guardó en teoria el cambio, vamos a ver para que  aparezca en la 
+web de git, por otro lado voy a ver que falta hacer en  git, ya sea un readme o un commit, aumque debe estra hehco
+
+
+'''
+
